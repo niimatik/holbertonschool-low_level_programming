8fd68862755d0d8bcd18ebf9cfdb198da59913d1
niimatik <11490@holbertonstudents.com>
2025-06-16 12:59:53 +0000

    task 6

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point of the program
+ *
+ *This program prints "Programming is like building a multilingual puzzle",
+ *followed by a new line.
+ *
+ *Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
