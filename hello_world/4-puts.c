ffe74a8873ada9b92269234d3ee04c5e349dfd11
niimatik <11490@holbertonstudents.com>
2025-06-16 12:58:10 +0000

    task 5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main-Entry point of the program
+ *
+ *This program prints "Programming is like building a multilingual puzzle",
+ *followed by a new line.
+ *
+ *Return:Always 0 (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
