a6c2fc39e16245720fb252687c7e568eba17a34e
niimatik <11490@holbertonstudents.com>
2025-06-17 13:10:59 +0000

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point of the program
+ *
+ *this program print the alphabet
+ *
+ *Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
