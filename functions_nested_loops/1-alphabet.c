6be3397d2a71a1841e951166f3d57613494994c6
niimatik <11490@holbertonstudents.com>
2025-06-18 09:06:50 +0000

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,15 +6,11 @@
  *
  *Return:Always 0 (success)
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	char c;
+    char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	_putchar('\n');
-	return (0);
+    for (c = 'a'; c <= 'z'; c++)
+        _putchar(c);
+    _putchar('\n');
 }
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,23 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
-void print_alphabet(void)
-	int main(void)
+int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-void print_alphabet(void)
-{
-	for (char c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
 }
