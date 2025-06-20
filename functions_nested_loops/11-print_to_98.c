73035f92afe8636348600a64aeb647ac7f3fc544
niimatik <11490@holbertonstudents.com>
2025-06-20 12:21:18 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -28,8 +28,14 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n > 98; n--)
+		for (; n >= 98; n--)
 		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
 			printf("%d, ", n);
 		}
 	}
