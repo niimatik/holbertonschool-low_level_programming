025e01a0baeba49545f7a8639d0cf6648448e838
niimatik <11490@holbertonstudents.com>
2025-06-23 08:36:08 +0000

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Print from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
