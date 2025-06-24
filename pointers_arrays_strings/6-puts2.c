a358356b2d0a2695e4961f3e911531db565d3d7a
niimatik <11490@holbertonstudents.com>
2025-06-24 13:39:23 +0000

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ *@str: string to print
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
