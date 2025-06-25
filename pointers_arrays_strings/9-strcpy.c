7a0b9ed0b751006bce78c7be07a897111d754cdf
niimatik <11490@holbertonstudents.com>
2025-06-25 08:09:11 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy a string
+ * @src: string to copie
+ * @dest: where to copie
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
