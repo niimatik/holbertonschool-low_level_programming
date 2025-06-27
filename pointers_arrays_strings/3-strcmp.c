bd253200b71efe8c9dbc2c62b6dbe71d367fb4dd
niimatik <11490@holbertonstudents.com>
2025-06-27 08:09:49 +0000

    task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ *
+ * Return:
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int vlr = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int lng;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		lng = a;
+	}
+	else
+	{
+		lng = b;
+	}
+
+	while (c <= lng)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			vlr = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (vlr);
+}
