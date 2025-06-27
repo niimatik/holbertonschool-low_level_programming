63e9ea56305f2c342d3a1e7984de086cdd2773f7
niimatik <11490@holbertonstudents.com>
2025-06-27 13:10:35 +0000

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+
+char *leet(char *s)
+{
+	int a = 0, b;
+	char ltr[] = {'A', 'E', 'O', 'T', 'L'};
+	int cfr[] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (ltr[b])
+		{
+			if (s[a] == ltr[b] || s[a] == (ltr[b] + 32))
+			{
+				s[a] = cfr[b];
+				b++;
+			}
+			else
+			{
+				b++;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
