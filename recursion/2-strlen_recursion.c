53f52d35d94dbf5e9e6c7cabff1fbc0224303bcd
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 <niimatik.gg@gmail.com>
2025-07-01 14:42:54 +0200

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-
+/**
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
 
 int _strlen_recursion(char *s)
 {
