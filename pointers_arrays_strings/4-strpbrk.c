64f216518a7d4f5081aa1d1ce8947e54f85ee3b0
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 <niimatik.gg@gmail.com>
2025-07-01 09:30:53 +0200

    task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include "main.h"
 
-
+/**
+ * _strsbrk - search a string
+ * @s: source string
+ * @accept: accepted characters
+ *
+ * Return: the string since the first found accepted character
+*/
 
 char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
