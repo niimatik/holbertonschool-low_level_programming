80919abdd37809f6c6b19ab4fd488146bc6aeeb9
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 <niimatik.gg@gmail.com>
2025-07-01 10:10:15 +0200

    task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  *
+  * Return: Nothing.
+*/
+
+void print_chessboard(char (*a)[8])
+{
+    int c, b;
+
+    for (c = 0; c < 8; c++)
+    {
+        for (b = 0; b < 8; b++)
+        {
+            _putchar(a[c][b]);
+        }
+        _putchar('\n');
+    }
+}
