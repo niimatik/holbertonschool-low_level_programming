e6e97b7d5c4ccc3f1209114eea78bab688711fe4
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 <niimatik.gg@gmail.com>
2025-07-02 15:44:06 +0200

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arg
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
