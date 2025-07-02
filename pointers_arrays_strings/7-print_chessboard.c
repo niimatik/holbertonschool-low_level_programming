2577b98ae74d6b51f8d4083ffe7e54bc0cf8ea89
niimatik <11490@holbertonstudents.com>
2025-07-02 07:28:25 +0000

    task 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -10,14 +10,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-    int c, b;
+	int c, b;
 
-    for (c = 0; c < 8; c++)
-    {
-        for (b = 0; b < 8; b++)
-        {
-            _putchar(a[c][b]);
-        }
-        _putchar('\n');
-    }
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
+		_putchar('\n');
+	}
 }
