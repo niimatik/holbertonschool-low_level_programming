cad24c7ac05dd442686bdfc778c179c12766d7b0
niimatik <11490@holbertonstudents.com>
2025-07-02 07:36:05 +0000

    task 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,20 +4,19 @@
 /**
  * factorial - return the factorial of a given number
  * @n: the number to calculate the factorial
- * 
+ *
  * Return: integer value
  */
 
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else if (n == 0)
-    {
-        return (1);
-    }
-
-    return (n * factorial(n - 1));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }
