270f6a63f2fb50b575daa6f67c62f145028abac1
niimatik <11490@holbertonstudents.com>
2025-07-02 07:26:07 +0000

    task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,23 +11,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int a = 0, b = 0;
+	int a = 0, b = 0;
 
-    while (haystack[a])
-    {
-        while (needle[b])
-        {
-            if (haystack[a + b] != needle[b])
-            {
-                break;
-            }
-            b++;
-        }
-        if (needle[b] == '\0')
-        {
-            return (haystack + a);
-        }
-        a++;
-    }
-    return ('\0');
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+			b++;
+		}
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+		a++;
+	}
+	return ('\0');
 }
