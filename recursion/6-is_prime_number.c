96072d9b1bc2f683d9e579678d29014da49f56e1
niimatik <11490@holbertonstudents.com>
2025-07-02 08:51:43 +0000

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+int is_prime(int n, int a);
+
+/**
+ * is_prime_number - Returns if a number is prime
+ * @n: the number to be checked
+ *
+ * Return: integer value
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime(n, 1));
+}
+
+/**
+ * is_prime - Check if number is prime
+ * @n: the number to be checked
+ * @i: the iteration times
+ *
+ * Return: 1 for prime or 0 composite
+ */
+
+int is_prime(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	if ((n / i) < i)
+	{
+		return (1);
+	}
+	return (is_prime(n, i + 1));
+}
