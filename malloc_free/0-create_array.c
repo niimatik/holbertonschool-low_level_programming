ea3dd2e40472e943bde571144883b1240ce482f9
niimatik <11490@holbertonstudents.com>
2025-07-07 08:40:57 +0000

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,24 +12,23 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int a;
-    char* b;
+	unsigned int a;
+	char* b;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    b = malloc(size * sizeof(char));
+	b = malloc(size * sizeof(char));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
 
-    if (b == NULL)
-    {
-        return (NULL);
-    }
-
-    for (a = 0; a < size; a++)
-    {
-        b[a] = c;
-    }
-    return (b);
+	for (a = 0; a < size; a++)
+	{
+		b[a] = c;
+	}
+	return (b);
 }
