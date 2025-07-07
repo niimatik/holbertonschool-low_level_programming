aad21f1c4d36599254509a729a3d48cb57ae70d2
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 <niimatik.gg@gmail.com>
2025-07-07 16:20:43 +0200

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0)
+		return (NULL);
+
+	if (height <= 0)
+		return (NULL);
+
+}
