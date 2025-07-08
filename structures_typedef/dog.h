278a64da55bda49e26b08b4357ededb81bf0dd3c
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 <niimatik.gg@gmail.com>
2025-07-08 16:01:47 +0200

    t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - structure représentant un chien
+ * @name: nom du chien (chaîne de caractères)
+ * @age: âge du chien (nombre à virgule flottante)
+ * @owner: nom du propriétaire du chien
+ *
+ * Description : Cette structure est utilisée pour stocker les informations
+ * de base sur un chien, notamment son nom, son âge et le nom de son propriétaire.
+ */
+
 struct dog
 {
 	char *name;
