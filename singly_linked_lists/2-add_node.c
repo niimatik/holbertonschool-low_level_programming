36414cf6149042223bcab4fae388ae2fc3b6eabe
niimatik <niimatik.gg@gmail.com>
2025-07-28 11:53:21 +0200

    docs: add of the task 2 and is prototype in the header file

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node at the beginning of a list
+ * @head: the list to add the node
+ * @str: the node to add
+ *
+ * Return: the address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *added;
+
+	if (head != NULL && str != NULL)
+	{
+		added = malloc(sizeof(list_t));
+		if (added == NULL)
+			return (NULL);
+
+		added->str = strdup(str);
+		added->len = _strlen(str);
+		added->next = *head;
+		*head = added;
+
+		return (added);
+	}
+	return (0);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+
+int _strlen(const char *s)
+{
+	int a = 0;
+
+	while (s)
+	{
+		s++;
+		a++;
+	}
+	return (a);
+}
