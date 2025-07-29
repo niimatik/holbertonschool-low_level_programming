08e8e009b2a4e552f5bfdbc51d387cb3aa775de1
niimatik <niimatik.gg@gmail.com>
2025-07-29 13:55:39 +0200

    docs: add of the task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free the list
+ * @head: list to free
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *freed;
+
+	while (head)
+	{
+		freed = head;
+		head = head->next;
+		free(freed);
+	}
+}
