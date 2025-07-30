aab1d8e5e215686cc7ab36f9f6446d638f0b61e0
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 <niimatik.gg@gmail.com>
2025-07-30 23:10:37 +0200

    docs: add of the header file and the task 0

---
+++ file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
