b2185e8141797272874725cca8543169f56658eb
niimatik <niimatik.gg@gmail.com>
2025-08-01 10:01:59 +0200

    fix: output of the function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,9 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content && write(fd, filename, _strlen(text_content)) == -1)
-	{
 		return (-1);
-	}
+
 	close(fd);
 	return (1);
 }
