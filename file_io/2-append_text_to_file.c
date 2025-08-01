c19b1092084b4c92f1fec79fe4c37a8330f999a2
niimatik <niimatik.gg@gmail.com>
2025-08-01 10:03:33 +0200

    fix: write call

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content && write(fd, filename, _strlen(text_content)) == -1)
+	if (text_content && write(fd, text_content, _strlen(text_content)) == -1)
 		return (-1);
 
 	close(fd);
