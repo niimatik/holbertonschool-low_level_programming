2fce454c6d7ad3d799c64689d054d7a89fcbf0e5
niimatik <niimatik.gg@gmail.com>
2025-08-01 11:40:08 +0200

    d

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,7 +35,7 @@ void copy_file(const char *src, const char *dest)
 	srcfd = open(src, O_RDONLY);
 	if (!src || srcfd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", src);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
 
