65e67c1fabb39c54bb445731579329038e9f8fd2
niimatik <niimatik.gg@gmail.com>
2025-08-01 09:41:14 +0200

    fix: put file into betty format

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-		write (fd, text_content, _strlen(text_content));
+		write(fd, text_content, _strlen(text_content));
 
-	close (fd);
+	close(fd);
 	return (1);
 }
 
