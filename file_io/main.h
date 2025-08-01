bc92c3c7a14dfa13610871cffa21e11030ecd819
niimatik <niimatik.gg@gmail.com>
2025-08-01 11:24:22 +0200

    docs: modified the header

--- file_io/main.h
+++ file_io/main.h
@@ -13,5 +13,6 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void copy_file (const char *src, const char *dest);
 
 #endif
