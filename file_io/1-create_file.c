36c4472410c25f81f0f15a3b19559039633c4ef7
niimatik <niimatik.gg@gmail.com>
2025-08-01 09:34:34 +0200

    docs: add of the task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with user only read and write permition
+ * @filename: the name of the file to create
+ * @text_content: the content of the file to write inside
+ *
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		write (fd, text_content, _strlen(text_content));
+
+	close (fd);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		a++;
+	}
+	return (a);
+}
